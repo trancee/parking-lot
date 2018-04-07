28c5cb0c9e48467d49e3d0ed532d66083af339c0
Philipp Grosswiler <philipp.grosswiler@ruma.co.id>
2018-04-07 17:12:31 +0700

    Removed main test.

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,3 +9,7 @@ class MainTestCase(unittest.TestCase):
 
     def tearDown(self):
         unittest.TestCase.tearDown(self)
+
+    @pytest.fixture(autouse=True)
+    def capsys(self, capsys):
+        self.capsys = capsys
