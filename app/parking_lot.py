b83c42fc08173b5092f418d397013b8e224f38d4
Philipp Grosswiler <philipp.grosswiler@ruma.co.id>
2018-04-07 16:23:26 +0700

    Removed storing separate vehicles.

--- app/parking_lot.py
+++ app/parking_lot.py
@@ -4,22 +4,26 @@ from app.vehicle import Vehicle
 class ParkingLot:
     def __init__(self, num_slots):
         self.__slots = [None] * num_slots
-        self.__vehicles = []
         print ('Created a parking lot with %d slots' % (len(self.__slots)))
 
     @property
     def slots(self):
         return self.__slots
-    @property
-    def vehicles(self):
-        return self.__vehicles
 
     def park(self, registration_number, colour):
         __vehicle = Vehicle(registration_number, colour)
 
-        if __vehicle in self.__vehicles:
-            print ('Vehicle already in parking slot %d' % (self.__slots.index(__vehicle)))
-            return False
+        try:
+            i = next(i for i, vehicle in enumerate(self.__slots) if vehicle and vehicle == __vehicle)
+
+            # Adjust the index
+            slot = i + 1
+
+            print ('Allocated slot number: %d' % (slot))
+            return True
+
+        except StopIteration:
+            pass
 
         try:
             i = next(i for i, vehicle in enumerate(self.__slots) if vehicle is None)
@@ -34,7 +38,6 @@ class ParkingLot:
         self.__slots[i] = __vehicle
         print ('Allocated slot number: %d' % (slot))
 
-        self.__vehicles.append(__vehicle)
         return True
 
     def leave(self, slot):
@@ -47,7 +50,6 @@ class ParkingLot:
             self.__slots[i] = None
             print ('Slot number %d is free' % (slot))
 
-            self.__vehicles.remove(__vehicle)
             return True
 
         return False
